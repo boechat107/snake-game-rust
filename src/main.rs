765826b73d71e7a3fb9d77bcb83c79128db4ebfc
Andre Ambrosio Boechat <boechat107@tutanota.com>
2022-08-30 16:58:48 -0300

    Place food and make the snake grow
    commit fa7c639b67c0f286abae42a3301698202ce4888f
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Tue Aug 30 16:31:26 2022 -0300
    
        Abstract the snake head update as a separate function
    
    commit 099e421d6d8f1d949322a9ae2065edb2c9eff9b4
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Tue Aug 30 16:21:05 2022 -0300
    
        Reduce the game grid size
    
    commit 0e1468025ad641f19391d5bd0b950a49a57f6ae7
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Tue Aug 30 16:20:34 2022 -0300
    
        Make the snake grow by not updating the tail when the head meets food
    
    commit 953787a30fb60edeee3240ac005b351d9337d96a
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Aug 29 21:30:30 2022 -0300
    
        Update all the game state in single function
    
    commit ac7ab51b05971415fe1fef78fcfc9e7545c685b3
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Aug 29 21:23:48 2022 -0300
    
        Add a function to place food in the game grid
    
    commit 132d3ae7ec1d02486597aa9a444ece9423f4d06f
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Aug 29 21:23:25 2022 -0300
    
        Add the crate "rand"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::{
     collections::VecDeque, io, io::Stdout, io::Write, thread, time::Duration,
 };
@@ -43,13 +44,31 @@ struct GameState {
     head_directions: VecDeque<SnakeDirection>,
 }
 
+const GRID_ROWS: usize = 15;
+const GRID_COLUMNS: usize = 30;
 const SNAKE: Cell = 1;
 const EMPTY: Cell = 0;
 const FOOD: Cell = 2;
 const QUIT_CHAR: char = 'q';
+const MAX_FOOD_AMOUNT: usize = 15;
 
 fn grid_size(grid: &Grid) -> (usize, usize) {
     (grid.len(), grid[0].len())
+}
+
+fn add_food(grid: &mut Grid, max_amount: usize) {
+    let (nrows, ncols) = grid_size(grid);
+    // A random number generator.
+    let mut rng = rand::thread_rng();
+    // This is a sequence of random food locations like [(x1, y1), (x2, y2)].
+    let food_locations = (0..max_amount)
+        .map(|_| (rng.gen_range(0..nrows), rng.gen_range(0..ncols)));
+    // We add the food to the grid, but only in empty cells.
+    for (food_x, food_y) in food_locations {
+        if grid[food_x][food_y] == EMPTY {
+            grid[food_x][food_y] = FOOD;
+        }
+    }
 }
 
 fn init_game_state(nrows: usize, ncols: usize) -> GameState {
@@ -125,7 +144,7 @@ fn refresh_screen(
     stdout.flush().unwrap();
 }
 
-fn calc_position(pos: CellPos, direction: SnakeDirection) -> (i16, i16) {
+fn _calc_position(pos: CellPos, direction: SnakeDirection) -> (i16, i16) {
     let (x, y) = (pos.0 as i16, pos.1 as i16);
     match direction {
         SnakeDirection::Right => (x, y + 1),
@@ -135,12 +154,37 @@ fn calc_position(pos: CellPos, direction: SnakeDirection) -> (i16, i16) {
     }
 }
 
+/// Updates the tail position by making its current cell empty and setting the
+/// new tail position.
+fn _update_snake_tail(state: &mut GameState) {
+    state.grid[state.tail.0][state.tail.1] = EMPTY;
+    state.tail = {
+        let (tx, ty) = _calc_position(
+            state.tail,
+            state.head_directions.pop_back().unwrap(),
+        );
+        (tx as usize, ty as usize)
+    };
+}
+
+fn _update_snake_head(
+    state: &mut GameState,
+    new_head_x: usize,
+    new_head_y: usize,
+    new_direction: SnakeDirection,
+) {
+    state.head = (new_head_x as usize, new_head_y as usize);
+    // Before updating the cell content with the snake's head, we store whas
+    state.grid[state.head.0][state.head.1] = SNAKE;
+    state.head_directions.push_front(new_direction);
+}
+
 fn update_snake(
     state: &mut GameState,
     new_direction: SnakeDirection,
 ) -> IsGameValid {
     let (nrows, ncols) = grid_size(&state.grid);
-    let (new_head_x, new_head_y) = calc_position(state.head, new_direction);
+    let (new_head_x, new_head_y) = _calc_position(state.head, new_direction);
     // If the snake's head position goes out of the board boundaries, the game
     // is over.
     if (new_head_x < 0)
@@ -150,20 +194,19 @@ fn update_snake(
     {
         return false;
     }
-    // The game is still valid. We update the head's position and its
-    // representation in the grid.
-    state.head = (new_head_x as usize, new_head_y as usize);
-    state.grid[state.head.0][state.head.1] = SNAKE;
-    state.head_directions.push_front(new_direction);
-    //We update the tail's position and representation in the grid.
-    state.grid[state.tail.0][state.tail.1] = EMPTY;
-    state.tail = {
-        let (tx, ty) = calc_position(
-            state.tail,
-            state.head_directions.pop_back().unwrap(),
-        );
-        (tx as usize, ty as usize)
-    };
+    // We know now that the new position is not out of the game grid. We can
+    // safely convert their types.
+    let (new_head_x, new_head_y) = (new_head_x as usize, new_head_y as usize);
+    let does_head_meets_food = state.grid[new_head_x][new_head_y] == FOOD;
+    // We update the head's position and its representation in the grid.
+    _update_snake_head(state, new_head_x, new_head_y, new_direction);
+    // We update the tail's position only if the head does not meet food. If it
+    // does, we want to make the snake grow.
+    if !does_head_meets_food {
+        _update_snake_tail(state);
+    }
+    // We keep track of the last direction.
+    state.last_direction = new_direction;
 
     true
 }
@@ -193,7 +236,8 @@ fn capture_input(stdin_keys: &mut Keys<AsyncReader>) -> Option<UserInput> {
 fn main() -> io::Result<()> {
     let mut stdout = io::stdout().into_raw_mode().unwrap();
     let mut stdin_keys = termion::async_stdin().keys();
-    let mut game = init_game_state(30, 60);
+    let mut game = init_game_state(GRID_ROWS, GRID_COLUMNS);
+    add_food(&mut game.grid, MAX_FOOD_AMOUNT);
 
     refresh_screen(&mut stdout, &String::from("Start"), &game.grid);
     for i in 0.. {
@@ -212,7 +256,6 @@ fn main() -> io::Result<()> {
         // We update the snake's position and check if it is valid (the snake
         // is inside the game board).
         let is_valid = update_snake(&mut game, new_direction);
-        game.last_direction = new_direction;
         // If the update is valid, we continue playing.
         if is_valid {
             refresh_screen(&mut stdout, &format!("Iteration {i}"), &game.grid);
@@ -227,4 +270,23 @@ fn main() -> io::Result<()> {
 
     write!(stdout, "{}", termion::cursor::Show).unwrap();
     Ok(())
+}
+
+#[cfg(test)]
+mod main_test {
+    use super::*;
+
+    #[test]
+    fn test_food_gen() {
+        let (nrows, ncols, nfood) = (10, 10, 5);
+        let mut game = init_game_state(nrows, ncols);
+        add_food(&mut game.grid, nfood);
+        print_grid(&game.grid);
+
+        let grid_sum: u8 = game.grid.iter().flat_map(|cols| cols.iter()).sum();
+        // The food is randomly placed in the grid, and it might be located in
+        // the snake place (therefore, ignored). The number of food in the grid
+        // may discount the initial snake size (which is 2).
+        assert!(grid_sum > (2 * SNAKE + (nfood as u8 - 2) * FOOD))
+    }
 }
